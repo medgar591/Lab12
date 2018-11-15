fcc26fd8028e06ac7e8c2333869ea57f8768fe45
Matt Edgar <Matthew.A.Edgar-1@ou.edu>
2018-11-15 15:50:53 -0600

    Initial test implementation, issues with testing points

--- RectangleTestOfficial.java
+++ RectangleTestOfficial.java
@@ -7,11 +7,79 @@ import org.junit.Test;
 /**
  * Test cases for the Rectangle
  * 
- * @author Stephen
- * @version 2018-04-02
- * Lab 11
+ * @author Stephen and Matt Edgar
+ * @version 2018-11-15 Lab 11
  */
 public class RectangleTestOfficial
 {
-    // TODO: implement this.
+    private static Rectangle r1 = new Rectangle(new Point(100, 100), 100, 50, Color.BLACK, true);
+    private static Point r1UL = new Point(50, 50);
+    private static Point r1LL = new Point(150, 50);
+    private static Point r1UR = new Point(50, 100);
+    private static Point r1LR = new Point(150, 100);
+    private static Rectangle r2 = new Rectangle(new Point(150, 150), 25, 25, Color.CYAN, false);
+    private static Point r2UL = new Point(138, 138);
+    private static Point r2LL = new Point(162, 138);
+    private static Point r2UR = new Point(138, 162);
+    private static Point r2LR = new Point(162, 162);
+
+    /**
+     * Right number of points?
+     */
+    @Test
+    public void testRectangleConstructorPointsArrayLength()
+    {
+        Point[] pts = r1.getLocation();
+        Assert.assertEquals(4, pts.length);
+    }
+
+    /**
+     * Check points
+     */
+    @Test
+    public void testRectangleConstructorCalculatedPoints()
+    {
+        Point[] pts1 = r1.getLocation();
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts1, r1UL));
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts1, r1LL));
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts1, r1LR));
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts1, r1UR));
+
+        Point[] pts2 = r2.getLocation();
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts2, r2UL));
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts2, r2LL));
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts2, r2LR));
+        Assert.assertTrue("Point in wrong location", ShapeUtils.pointInSet(pts2, r2UR));
+
+    }
+
+    /**
+     * Unfilled case
+     */
+    @Test
+    public void testRectangleConstructorUnfilled()
+    {
+        Assert.assertFalse("Fill incorrect.", r2.isFilled());
+    }
+
+    /**
+     * Filled case
+     */
+    @Test
+    public void testRectangleConstructorFilled()
+    {
+        Assert.assertTrue("Fill incorrect.", r1.isFilled());
+    }
+
+    /**
+     * Color
+     */
+    @Test
+    public void testRectangleConstructorColor()
+    {
+        Color clr1 = r1.getColor();
+        Color clr2 = r2.getColor();
+        Assert.assertEquals("Color incorrect.", Color.BLACK, clr1);
+        Assert.assertEquals("Color incorrect.", Color.CYAN, clr2);
+    }
 }
